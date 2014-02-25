c0ff343780e716cfdf142cc9346dee0227839edd
StefanPATRICE <stefan.patrice@gmail.com>
2014-02-25 15:59:45 +0100

    Paralax

---
+++ prototypage/paralax_giroscope/Assets/giro_camera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class giro_camera : MonoBehaviour {
+
+    int i = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(i > 1000)
+			i = 0;
+		else
+			i++;
+
+        transform.position = new Vector3(
+            (float) i / 150f,
+            Mathf.Cos(Input.acceleration.y) * 2f,
+            0f
+        );
+	}
+}
---
+++ prototypage/paralax_giroscope/Assets/move_camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_camera : MonoBehaviour {
+
+    int i = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(i > 100)
+			i = 0;
+		else
+			i++;
+
+		transform.position = new Vector3((float) i / 100f, 0, 0);
+	}
+}
