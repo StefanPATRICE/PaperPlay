b7ee39a73a26e4011a9ecf97a847b55a1c404910
StefanPATRICE <stefan.patrice@gmail.com>
2014-02-26 09:01:13 +0100

    Paralax

--- prototypage/paralax_giroscope/Assets/giro_camera.cs
+++ prototypage/paralax_giroscope/Assets/giro_camera.cs
@@ -18,9 +18,8 @@ public class giro_camera : MonoBehaviour {
 
 		accelerationY = Mathf.Abs(Input.acceleration.y) * 0.3f + accelerationY * 0.7f;
 
-
         transform.position = new Vector3(
-            (float) Mathf.Sin(i / 200f) * 6,
+            (float) Mathf.Sin(i / 300f) * 6,
             Mathf.Cos(accelerationY) * 1f + 1,
             0f
         );
